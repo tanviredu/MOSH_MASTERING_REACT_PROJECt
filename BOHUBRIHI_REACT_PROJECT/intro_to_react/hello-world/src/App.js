0bc6ec3a9573148a26a7576bcae833656556a26b
Tanvir Rahman <tanvirrahman@pop-os.localdomain>
2020-09-16 20:08:43 +0600

    added

--- BOHUBRIHI_REACT_PROJECT/intro_to_react/hello-world/src/App.js
+++ BOHUBRIHI_REACT_PROJECT/intro_to_react/hello-world/src/App.js
@@ -5,16 +5,29 @@ import './App.css';
 // this is a function based component
 
 function App() {
+
+  // the return will be jsx
+  // we can do it jsvascript 
+
+
   return (
     <div className="App">
       <h1 className="display-4">
         This is the main Component
       </h1>
       <Person />
-      
+
       <Person />
     </div>
   );
+
+  // first parameter is the 
+  // element and other is the additional settings
+
+  // this is the javascript way
+  // but this is not the easy way
+  // return React.createElement('div',{"className":"App"},React.createElement('h1',null,"hello world"),<Person />);
+
 }
 
 
@@ -28,6 +41,8 @@ function Person(){
     
 
     );
+
+  // return React.createElement('div',null,React.createElement('h1',null,"This is a child component"))
 }
 
 
