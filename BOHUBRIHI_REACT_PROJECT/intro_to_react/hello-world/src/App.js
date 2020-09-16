3bf06de223e55cbc9e4cbff5bffcd922ba57da89
Tanvir Rahman <tanvirrahman@pop-os.localdomain>
2020-09-16 20:25:33 +0600

    added

--- BOHUBRIHI_REACT_PROJECT/intro_to_react/hello-world/src/App.js
+++ BOHUBRIHI_REACT_PROJECT/intro_to_react/hello-world/src/App.js
@@ -1,51 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import Person from './components/Person'
+
+// function App() {
 
 
-// this is a function based component
+//   return (
+//     <div className="App">
+//       <h1 className="display-4">
+//         This is the main Component
+//       </h1>
+//       <Person />
+//       <Person />
+//     </div>
+//   );
+// }
 
-function App() {
-
-  // the return will be jsx
-  // we can do it jsvascript 
-
-
-  return (
-    <div className="App">
+// use the class based component
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
       <h1 className="display-4">
         This is the main Component
       </h1>
       <Person />
-
       <Person />
     </div>
-  );
-
-  // first parameter is the 
-  // element and other is the additional settings
-
-  // this is the javascript way
-  // but this is not the easy way
-  // return React.createElement('div',{"className":"App"},React.createElement('h1',null,"hello world"),<Person />);
-
-}
-
-
-// make anohter componen here
-function Person(){
-  return(
-    <div>
-      <h1> This is a child Component </h1>
-
-    </div>
-    
-
     );
-
-  // return React.createElement('div',null,React.createElement('h1',null,"This is a child component"))
+  }
 }
 
-
-// this app is exported here and then it will be
-// imported from the index js
 export default App;
---
+++ BOHUBRIHI_REACT_PROJECT/intro_to_react/hello-world/src/components/Person.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// make anohter componen here
+function Person(){
+  return(
+    <div>
+      <h1> This is a child Component </h1>
+    </div>
+    );
+}
+
+
+export default Person;
