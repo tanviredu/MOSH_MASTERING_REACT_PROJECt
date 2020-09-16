7f94a0f828196d276f79694442b813729de116c7
Tanvir Rahman <tanvirrahman@pop-os.localdomain>
2020-09-16 20:58:55 +0600

    added

--- BOHUBRIHI_REACT_PROJECT/intro_to_react/hello-world/src/App.js
+++ BOHUBRIHI_REACT_PROJECT/intro_to_react/hello-world/src/App.js
@@ -2,9 +2,11 @@ import React, {Component} from 'react';
 import './App.css';
 import Person from './components/Person'
 
+
+// this is function based component
+
+
 // function App() {
-
-
 //   return (
 //     <div className="App">
 //       <h1 className="display-4">
@@ -16,7 +18,9 @@ import Person from './components/Person'
 //   );
 // }
 
-// use the class based component
+
+// this is clas based component
+
 class App extends Component{
   render(){
     return(
@@ -24,8 +28,11 @@ class App extends Component{
       <h1 className="display-4">
         This is the main Component
       </h1>
-      <Person />
-      <Person />
+      <Person name = "Rahim" age="30" />
+      <Person name = "kerim" age="32" />
+      <Person name = "javed" age="33" />
+      <Person name = "Tanvir" age="34" />
+      
     </div>
     );
   }
--- BOHUBRIHI_REACT_PROJECT/intro_to_react/hello-world/src/components/Person.js
+++ BOHUBRIHI_REACT_PROJECT/intro_to_react/hello-world/src/components/Person.js
@@ -1,12 +1,76 @@
-import React from 'react'
-// make anohter componen here
-function Person(){
-  return(
-    <div>
-      <h1> This is a child Component </h1>
-    </div>
-    );
+import React, {Component} from 'react'
+import "./Person.css"
+
+// this is a function based component
+
+// function Person(){
+//   return(
+//     <div>
+//       <h1> This is a child Component </h1>
+//     </div>
+//     );
+// }
+
+
+// this is a component with the arrow function
+// ununsed components will throw an error
+// var Person2 = ()=>{
+//   return(
+//     <div>
+//       <h1> This is a child Component </h1>
+//     </div>
+//   );
+// }
+
+// Data is passing through the props
+// from the main component through child component
+// var Person = (props)=>{
+//   console.log(props);
+//   return ( 
+//     <div className="Person">
+//       <div className="jumbotron">
+//           <h1 className="display-4"> Name : {props.name} </h1>
+//           <p className="h4">  Age  : {props.age} </p>
+//       </div>
+//     </div> 
+
+//   )
+// }
+
+
+// in the class the peops will be a property
+// in the component
+// in order to get the peops
+// you need to call the super method
+// to get the peops properties
+
+
+class Person extends Component{
+  
+  // in order to use the props
+  // you have to called the super method inside the 
+  // the constrtor
+  constructor(props){
+    super(props);
+  }
+  render(){
+    // console.log(this.props);
+    return (
+    <div className="Person">
+      <div className="jumbotron">
+          <h1 className="display-4"> Name : {this.props.name} </h1>
+          <p className="h4">  Age  : {this.props.age} </p>
+      </div>
+    </div> 
+)
+    
+  }
 }
+
+
+// now chaing the person component with the class component
+// will render the same thing with person2
+
 
 
 export default Person;
